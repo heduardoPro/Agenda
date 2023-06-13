4db15bc28506e1cac497e72e8208c4930c3a84ad
heduardoPro <heduardocr7@gmail.com>
2023-06-13 11:21:27 -0300

    replacing contact views with contact_views and editing the config

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404  
+from contact.models import Contact
+from django.http import Http404
 
 # Create your views here.
 def index(request):
+    contacts = Contact.objects.filter(show=True).order_by('-id')[:10] #fatiamento
+
+    print(contacts.query)
+    context = {
+        'contacts': contacts,
+    }
+
     return render(
         request,
         'contact/index.html',
+        context, 
+    )
+
+def contact(request, contact_id):
+    #single_contact = Contact.objects.filter(pk=contact_id).first() #fatiamento
+    # #if single_contact is None:
+    #    raise Http404
+
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
+        context, 
     )
