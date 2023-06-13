0f6198d33f31e5932cb7a88c0850fb18955104f0
heduardoPro <heduardocr7@gmail.com>
2023-06-13 17:25:10 -0300

    Creating the URL, view and template to create a contact CREATE

---
+++ contact/views/contact_form.py
@@ -0,0 +1,15 @@
+
+from django.shortcuts import render
+from contact.models import Contact
+
+
+def create(request):
+
+    context = {
+        
+    }
+    return render(
+        request,
+        'contact/create.html',
+        context,
+    )
