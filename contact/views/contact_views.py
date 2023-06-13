f0cb684505c01abc7fee5492adafa0b370b8fb47
heduardoPro <heduardocr7@gmail.com>
2023-06-13 12:08:05 -0300

    method search

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404  
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, redirect 
 from contact.models import Contact
-from django.http import Http404
+
 
 # Create your views here.
 def index(request):
@@ -17,6 +18,33 @@ def index(request):
         request,
         'contact/index.html',
         context, 
+    )
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
     )
 
 def contact(request, contact_id):
