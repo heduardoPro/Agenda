91d5ce45da0264bc6f279378bef309ce67f34710
heduardoPro <heduardocr7@gmail.com>
2023-06-20 19:43:16 -0300

    criando forms.py e fazendo as imports necessarias

--- contact/views/contact_form.py
+++ contact/views/contact_form.py
@@ -1,15 +1,7 @@
 from django.shortcuts import render
-from contact.models import Contact
-from django import forms
+from contact.forms import ContactForm
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            'first_name',
-            'last_name',
-            'phone',
-        )
+
 
 def create(request):
 
